7c49a7d0f64fc85c29a77ac6b0877abbd9e113d4
wwsheng009 <vincentwwsheng@gmail.com>
2023-04-10 16:48:33 +0800

    删除没用代码

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,8 @@ rm -rf ${target}/studio/*
 cp -rf dist_esm/app/studio/* ${target}/studio/
 
 if [ -d "dist_esm/app/scripts/ddic" ]; then
+  mkdir -p ${target}/ddic
+  mkdir -p ${target}/file
  cp -rf dist_esm/app/scripts/ddic/* ${target}/scripts/ddic/
  cp -rf dist_esm/app/scripts/file/* ${target}/scripts/file/
 fi
